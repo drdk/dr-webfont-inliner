3ac67842344330266324dfe19d4a965a2ce53c1e
rafl <rafl@dr.dk>
2013-09-27 14:14:28 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+var fs = require("fs"),
+	path = require("path");
+
+if (require.main === module) {
+	var optimist = require("optimist")
+	.usage("Inlines webfonts in stylesheets.\nUsage: node ./index.js -o [file] input")
+	.alias('o', 'output')
+	.check(function(argv){
+		if (!("_" in argv) || !argv._.length) {
+			throw "Input param missing!";
+		}
+	}),
+	argv = optimist.argv;
+	inline(argv._[0], argv.output || null);
+}
+
+function inline (input, output) {
+	input = input.replace(/\\/g, "/");
+	var root = input.replace(/\/[^\/]+$/, "/"),
+		output = output || input.replace(/(\.[^.]+)$/, "-inline$1"),
+		css = fs.readFileSync(input, {encoding: "utf8"});
+	css = css.replace(/(url\(["']?)([^"']+.(woff|ttf|eot|svg))(?:\??#[^"']+)?(["']?\))/g, function (m, cssPrefix, url, type, cssSuffix) {
+		url = path.resolve(root, url);
+		var data = fs.readFileSync(url),
+			mime = {
+				woff: "application/x-font-woff",
+				ttf: "application/x-font-ttf",
+				eot: "application/x-font-eot",
+				svg: "image/svg+xml"
+			}[type];
+		return cssPrefix + "data:" + mime + ";base64," + data.toString('base64') + cssSuffix;
+	});
+
+	fs.writeFileSync(output, css);
+}
+
+module.exports = inline;
